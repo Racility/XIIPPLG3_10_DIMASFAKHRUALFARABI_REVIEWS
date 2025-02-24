d3cb1fefeeca1bb078cf0afae27ad9f5cd4f2d1d
Racility <dimasfakhrul2@gmail.com>
2025-02-24 12:53:37 +0700

    book first

--- router/reviews.js
+++ router/reviews.js
@@ -4,18 +4,18 @@ const router = express.Router();
 const reviewcontroller = require('../controllers/review')
 
 // GET: Ambil semua ulasan
-router.get("/reviews", reviewcontroller.get);
+router.get("/reviews", reviewcontroller.getReviews);
 
 // GET: Ambil ulasan berdasarkan ID
-router.get("/review/:id", reviewcontroller.get1);
+router.get("/review/:id", reviewcontroller.getReviewById);
 
 // POST: Tambahkan ulasan baru
-router.post("/review", reviewcontroller.post);
+router.post("/review", reviewcontroller.createReview);
 
 // PUT: Update ulasan berdasarkan ID
-router.put("/review/:id", reviewcontroller.put);
+router.put("/review/:id", reviewcontroller.updateReview);
 
 // DELETE: Hapus ulasan berdasarkan ID
-router.delete("/review/:id", reviewcontroller.delete);
+router.delete("/review/:id", reviewcontroller.deleteReview);
 
 module.exports = router;
